7dd94563dc61f9443afe377bedd361ca61c5746e
VOLKANO-git <alaamim555@gmail.com>
2026-09-18 10:52:05 +0100

    help me

--- Desktop/loops/challenge1.js
+++ Desktop/loops/challenge1.js
@@ -3,5 +3,5 @@ let a = parseInt(prompt("enter the number for table of multiplication: "));
 console.log('table of multiplication of ${a}:');
 for (let i = 1; i <=10;i++){
     let result = a * i ;
-    console.log(`${a} * ${i} = ${reulst}`);
+    console.log(`${a} * ${i} = ${result}`);
 }
