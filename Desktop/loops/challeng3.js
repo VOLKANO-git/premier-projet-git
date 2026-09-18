84e086d8e9b59f1c6aacd37129e5d1f5d4a60601
VOLKANO-git <alaamim555@gmail.com>
2026-09-18 11:23:44 +0100

    another W goes to

---
+++ Desktop/loops/challeng3.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+let n = parseInt(prompt("Enter a positive integer: "));
+let sum = 0; 
+for (let i = 1; i <= n; i++) {
+    sum += i;
+}
+console.log(`The sum of the first ${n} positive integers is: ${sum}`);
