caa56909f35c8e8ab913291f940834526ae2edc2
VOLKANO-git <alaamim555@gmail.com>
2026-09-19 09:58:26 +0100

    that was ez

---
+++ Desktop/arrays/ch1.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+const numbers = [1,2,3,4,5];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
