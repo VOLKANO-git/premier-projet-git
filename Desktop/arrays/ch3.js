8163af84003ac831f60bde3a315781c1c798caf2
VOLKANO-git <alaamim555@gmail.com>
2026-09-19 15:04:30 +0100

    fixing a mistake

--- Desktop/arrays/ch3.js
+++ Desktop/arrays/ch3.js
@@ -1,5 +1,5 @@
 const prompt = require('prompt-sync')();
-let somme = 0;
+const somme = 0;
 for (let i = 0; i < 3; i++) {
   const num = parseInt(prompt(`Enter number ${i + 1}: `));
   somme += num;
