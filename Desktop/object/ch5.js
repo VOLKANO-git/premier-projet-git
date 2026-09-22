908c489b1c46d0fbedb1d793e50b4f4a780fe735
VOLKANO-git <alaamim555@gmail.com>
2026-09-22 12:07:06 +0100

    empty

---
+++ Desktop/object/ch5.js
@@ -0,0 +1,8 @@
+const prompt = require ('prompt-sync');
+const book = {
+    title: 'The Great Gatsby',
+    writer: 'F. Scott Fitzgerald',
+    year: 1925,
+}; function addition(title,writer,year){
+    return (book)
+}console.log(addition(book));
