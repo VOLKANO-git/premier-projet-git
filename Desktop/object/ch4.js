2979cae2273271ddb191c31f9eea9ddf96ae855f
VOLKANO-git <alaamim555@gmail.com>
2026-09-22 10:44:25 +0100

    empty

---
+++ Desktop/object/ch4.js
@@ -0,0 +1,9 @@
+const prompt = require ('prompt-sync');
+const point = {
+   x:10,
+   y:9
+}; 
+   console.log(point.x,point.y);
+   point.x = 19;
+   point.y = 20;
+   console.log(point.x,point.y);
