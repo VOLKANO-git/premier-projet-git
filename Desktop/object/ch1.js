11cc29732e86a135d076dade14ffef9bcba191fc
VOLKANO-git <alaamim555@gmail.com>
2026-09-22 09:41:52 +0100

    empty

---
+++ Desktop/object/ch1.js
@@ -0,0 +1,8 @@
+const prompt = require ('prompt-sync');
+const person = {
+    lastname : "laamim",
+    firstname : "anas",
+    age : 20 ,
+}; console.log(person.lastname);
+   console.log(person.firstname);
+   console.log(person.age);
